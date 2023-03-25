745b0ee21071312f7ac237967d65464b617ccecd
gosusleeper <davidswim760@gmail.com>
2023-03-25 16:29:19 -0700

    Registration Service MQ initial code

--- cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
+++ cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
@@ -1,12 +1,17 @@
 package com.cst438.services;
 
 
+import java.util.ArrayList;
+
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.cst438.domain.AssignmentGrade;
 import com.cst438.domain.Course;
 import com.cst438.domain.CourseDTOG;
 import com.cst438.domain.CourseRepository;
@@ -44,8 +49,34 @@ public class RegistrationServiceMQ extends RegistrationService {
 	@Transactional
 	public void receive(EnrollmentDTO enrollmentDTO) {
 		
+		System.out.println("Receive enrollment :" + enrollmentDTO);
 		//TODO  complete this method in homework 4
 		
+		//get message from q, get enrollment entity and save to grade book database asynchronous
+			
+		//EnrollmentDTO e = enrollmentDTO;
+		Course c = courseRepository.findById(enrollmentDTO.course_id).orElse(null);
+
+		int course_id = enrollmentDTO.course_id;
+		
+		CourseDTOG cdto = new CourseDTOG();
+		cdto.course_id = course_id;
+		cdto.grades = new ArrayList<>();
+		for (Enrollment e: c.getEnrollments()) {
+			double total=0.0;
+			int count = 0;
+			for (AssignmentGrade ag : e.getAssignmentGrades()) {
+				count++;
+				total = total + Double.parseDouble(ag.getScore());
+			}
+			double average = total/count;
+			CourseDTOG.GradeDTO gdto = new CourseDTOG.GradeDTO();
+			gdto.grade=letterGrade(average);
+			gdto.student_email=e.getStudentEmail();
+			gdto.student_name=e.getStudentName();
+			cdto.grades.add(gdto);
+			System.out.println("Course="+course_id+" Student="+e.getStudentEmail()+" grade="+gdto.grade);
+		}
 	}
 
 	// sender of messages to Registration Service
@@ -53,7 +84,24 @@ public class RegistrationServiceMQ extends RegistrationService {
 	public void sendFinalGrades(int course_id, CourseDTOG courseDTO) {
 		 
 		//TODO  complete this method in homework 4
+
+		String course_id_string = Integer.toString(course_id);
 		
+		rabbitTemplate.convertAndSend( course_id_string, courseDTO);
+		
+		System.out.println("Message send to registration service for courseDTO "+ courseDTO);  
 	}
 
-}
+	
+	private String letterGrade(double grade) {
+		if (grade >= 90) return "A";
+		if (grade >= 80) return "B";
+		if (grade >= 70) return "C";
+		if (grade >= 60) return "D";
+		return "F";
+	}	
+	
+	
+	
+	}
+
